664d7b0407dc40a3baddd23df33587fbc246b9b8
199ocero <199ocero@gmail.com>
2023-05-07 16:43:52 +0800

    add helper functions

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -9,3 +9,10 @@ if (! function_exists('snakeCase')) {
         return $snakeCase;
     }
 }
+
+if(!function_exists('generateUrl')){
+    function generateUrl($base_url, $version, $endpoint)
+    {  
+        return rtrim($base_url, '/') . '/' . trim($version, '/') . '/' . trim($endpoint, '/');
+    }
+}
