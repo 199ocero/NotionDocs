126a2b14f895c98cf57f455f9d80917726e55fed
199ocero <199ocero@gmail.com>
2023-05-16 07:03:53 +0800

    add rules

---
+++ app/Rules/EndpointValidationRule.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class EndpointValidationRule implements Rule
+{
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        return preg_match('/^[a-zA-Z0-9\/]+$/', $value);
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The :attribute must be a valid endpoint.';
+    }
+}
