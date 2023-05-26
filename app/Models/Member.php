fa292dc0721e1b72ced62dc57f5341a0ef3b2261
199ocero <199ocero@gmail.com>
2023-05-26 17:15:48 +0800

    add member resource, model and migration

---
+++ app/Models/Member.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Member extends Model
+{
+    use HasFactory;
+
+    protected $table = 'members';
+    protected $fillable = [
+        'team_id',
+        'invited_by_id',
+        'invited_id',
+        'status',
+    ];
+}
---
+++ database/migrations/2023_05_26_083720_create_members_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('team_id');
+            $table->unsignedBigInteger('invited_by_id');
+            $table->unsignedBigInteger('invited_id');
+            $table->enum('status', ['pending', 'accepted', 'rejected'])->default('pending');
+            $table->timestamps();
+            $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');
+            $table->foreign('invited_by_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('invited_id')->references('id')->on('users')->onDelete('cascade');
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+};
