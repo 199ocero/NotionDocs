ecb724dd2d818e5f7c9d8590f14c67c7c4134c3c
199ocero <199ocero@gmail.com>
2023-06-02 06:38:02 +0800

    add helper for getting the team

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\Team;
+use App\Models\Member;
+
 
 if (! function_exists('snakeCase')) {
     function snakeCase($string)
@@ -14,5 +17,13 @@ if(!function_exists('generateUrl')){
     function generateUrl($base_url, $version, $endpoint)
     {  
         return rtrim($base_url, '/') . '/' . trim($version, '/') . '/' . trim($endpoint, '/');
+    }
+}
+
+if(!function_exists('getTeam')){
+    function getTeam()
+    {  
+        $member = Member::where('invited_id', auth()->user()->id)->where('status', Member::ACCEPTED)->first();
+        return Team::where('user_id', $member->invited_by_id)->first();
     }
 }
